e47bb9b2c5f2820cdf10eda46122ab974e7a0e3f
Iwan-Zotow <iwanzotow@gmail.com>
2015-10-07 00:01:36 -0400

    ad optional # of original events

--- g2b.py
+++ g2b.py
@@ -20,7 +20,7 @@ def load_events(filename, energy_thr = 0.01):
     :param energy_thr: energy threshold, events with energy below threshold will be thrown out
     """
     events = []
-    
+
     with open(filename) as f:
         for line in f:
 
@@ -29,16 +29,16 @@ def load_events(filename, energy_thr = 0.01):
             e    = []
             for n in s:
                 e.append(float(n))
-                
+
             if len(e) == 8:
                 if e[1] > energy_thr:
                     events.append(e)
-            
+
     if len(events) == 0:
         return None
-        
+
     return events
-    
+
 def shift_back(e, zshift):
     """
     Project back particles from current plane to plane shifted by zshift
@@ -48,40 +48,44 @@ def shift_back(e, zshift):
     X = e[2]
     Y = e[3]
     Z = e[4]
-    
+
     wx = e[5]
     wy = e[6]
     wz = e[7]
-    
+
     s = 0.0
     if wz != 0.0:
         s = zshift / wz
-        
+
     return (X-wx*s, Y-wy*s, Z-wz*s)
 
-    
+
 def write_record_long(e, zshift, f):
     """
     Write MODE2 PhSF particle record
+
+    :param e: an event
+    :param zshift: shift value, backward, in mm
+    :param f: file
     """
 
-    LATCH = 8388608
+    LATCH = 8388608 # magic value, check PIRS-509 for description of bits in LATCH bitmask
     f.write(struct.pack("i", LATCH))
 
     E = -np.float32(e[1])
     f.write(struct.pack("f", E))
-    
+
     (xx, yy, zz) = shift_back(e, zshift)
 
     X = np.float32(mm2cm(xx))
     f.write(struct.pack("f", X))
-    
+
     Y = np.float32(mm2cm(yy))
     f.write(struct.pack("f", Y))
-    
+
     U = np.float32(e[5])
     f.write(struct.pack("f", U))
-    
+
     V = np.float32(e[6])
     f.write(struct.pack("f", V))
 
@@ -89,7 +93,7 @@ def write_record_long(e, zshift, f):
     if e[7] < 0.0: # keep W sign
         WT = np.float32(-WT)
     f.write(struct.pack("f", WT))
-    
+
     ZLAST = np.float32(mm2cm(e[4] - 150.00)) # back by 150mm!
     f.write(struct.pack("f", ZLAST))
 
@@ -98,11 +102,11 @@ def write_beam_long(header, events, zshift, filename):
     """
     Write BEAMnrc phase space file, MODE2 format
     """
-    
+
     (mode, NPPHSP, NPHOTPHSP, EKMAX, EKMIN, NINCP) = header
-    
+
     with open(filename, "wb") as f:
-        f.write(mode) 
+        f.write(mode)
         f.write(struct.pack("i", NPPHSP))
         f.write(struct.pack("i", NPHOTPHSP))
 
@@ -114,23 +118,26 @@ def write_beam_long(header, events, zshift, filename):
 
         tmp = np.float32(NINCP)
         f.write(struct.pack("f", tmp))
-        
+
         dummy = b"XXXXXXX" # 7 bytes to fill header up to 32bytes
         f.write(dummy)
-        
+
         for e in events:
             write_record_long(e, zshift, f)
-    
+
 def make_header(nof_original, events):
     """
     Produce  from Geant data header for BeamNRC
+
+    :param nof_original: number of original events used to get events in G4 phase space file
+    :param events: all events from G4 phase space file
     """
     mode = b"MODE2"
-    
+
     NPPHSP = len(events) # total nof of particle records
 
     NPHOTPHSP =  len(events) # total nof of photon records
-    
+
     EKMAX = -1.0        # max kinetic energy, MeV
     EKMIN = 999999999.0 # min e- kinetic energy, MeV
     for e in events:
@@ -139,22 +146,29 @@ def make_header(nof_original, events):
             EKMAX = energy
         if energy < EKMIN:
             EKMIN = energy
-            
+
     EKMIN = 0.183944478631 # from previous PhSF
-    
+
     NINCP = float(nof_original)
-    
+
     return (mode, NPPHSP, NPHOTPHSP, EKMAX, EKMIN, NINCP)
-    
+
 def main():
+    """
+    Process Geant phase space file and build BEAMnrc phase space file
+    """
 
     if len(sys.argv) < 3:
-        print("g2b input_fname output_phsf_name_without_extension")
+        print("g2b input_fname output_phsf_name_without_extension <optional # of original events>")
         return
+
+    nof_original = 10000000000
+    if len(sys.argv) > 3:
+        nof_original = int(sys.argv[3])
 
     events = load_events(sys.argv[1])
 
-    header = make_header(10000000000, events)
+    header = make_header( nof_original, events)
     (mode, NPPHSP, NPHOTPHSP, EKMAX, EKMIN, NINCP) = header
 
     print(mode, NPPHSP, NPHOTPHSP, EKMAX, EKMIN, NINCP)
