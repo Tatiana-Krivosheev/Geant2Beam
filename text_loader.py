3d97cf4edf387755ea8a2c9664390b02ea3dc17d
Nobody <nobody@nowhere.to.go>
2017-11-01 21:35:41 -0400

    Load text events with filtering

--- text_loader.py
+++ text_loader.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 
-def load_events(filename, energy_thr = 0.01):
+def load_events(filename, energy_thr = 0.01, nof_events = -1):
     """
-    load all events from a text file (W,E,X,Y,Z,WX,WY,WZ), remove events with energy below threshold
+    load photon events from a text file (W,E,X,Y,Z,WX,WY,WZ), remove events with energy below threshold
     :param filename: file name to load events from
     :param energy_thr: energy threshold, events with energy below threshold will be thrown out
     """
     events = []
 
+    k = 0
     with open(filename, "r+") as f:
         for line in f:
 
@@ -16,20 +17,28 @@ def load_events(filename, energy_thr = 0.01):
             s = line.split()
             s = [x for x in s if x]  # remove empty lines
 
-            WT = float(s[0])
-            E  = float(s[1])
+            if "GGG" not in s[0]:
+                continue
+
+            WT = float(s[1])
+            E  = float(s[2])
             if E < energy_thr:
                 break
 
-            X = float(s[2])
-            Y = float(s[3])
-            Z = float(s[4])
+            X = float(s[3])
+            Y = float(s[4])
+            Z = float(s[5])
 
-            WX = float(s[5])
-            WY = float(s[6])
-            WZ = float(s[7])
+            WX = float(s[6])
+            WY = float(s[7])
+            WZ = float(s[8])
 
             e = (W,E,X,Y,Z,WX,WY,WZ)
             events.append(e)
+
+            k += 1
+            if nof_events > 0:
+                if k == nof_events:
+                    break
 
     return events if len(events) > 0 else None
